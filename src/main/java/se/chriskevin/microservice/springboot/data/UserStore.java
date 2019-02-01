17a44514b7724e9421998576822dc5798ff3a5f3
Chris Sundberg <chris.sundberg89@gmail.com>
2019-02-02 00:27:51 +0100

    Initial test setup

--- src/main/java/se/chriskevin/microservice/springboot/data/UserStore.java
+++ src/main/java/se/chriskevin/microservice/springboot/data/UserStore.java
@@ -1,6 +1,7 @@
 package se.chriskevin.microservice.springboot.data;
 
 import io.vavr.collection.List;
+import io.vavr.control.Option;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,22 +16,24 @@ public class UserStore {
   private List<User> users =
       List.of(new User(UUID.randomUUID(), "Chris"), new User(UUID.randomUUID(), "Kevin"));
 
+  public Option<User> getUser(final UUID uuid) {
+    return this.users.find(user -> user.getId().equals(uuid));
+  }
+
   public User addUser(final User user) {
     this.users = this.users.append(user);
     return user;
   }
 
-  public UUID removeUser(final UUID uuid) {
-    this.users = this.users.remove(unsafeGetUser(uuid));
-    return uuid;
+  public Option<User> removeUser(final UUID uuid) {
+    final Option<User> user = getUser(uuid);
+    this.users = user.map(x -> this.users.remove(x)).getOrElse(this.users);
+    return user;
   }
 
-  public User updateUser(final UUID uuid, final User updatedUser) {
-    this.users = this.users.replace(unsafeGetUser(uuid), updatedUser);
-    return unsafeGetUser(uuid);
-  }
-
-  private User unsafeGetUser(final UUID uuid) {
-    return this.users.find(user -> user.getId().equals(uuid)).get();
+  public Option<User> updateUser(final UUID uuid, final User updatedUser) {
+    final Option<User> user = getUser(uuid);
+    this.users = user.map(x -> this.users.replace(x, updatedUser)).getOrElse(this.users);
+    return user;
   }
 }
