7f987ef2abb08a71aace398986055110ae92d49f
Chris Sundberg <chris.sundberg89@gmail.com>
2019-02-02 00:27:51 +0100

    Initial test setup

--- src/main/java/se/chriskevin/microservice/springboot/service/UserService.java
+++ src/main/java/se/chriskevin/microservice/springboot/service/UserService.java
@@ -4,33 +4,37 @@ import io.vavr.collection.List;
 import io.vavr.control.Option;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import se.chriskevin.microservice.springboot.data.UserStore;
 import se.chriskevin.microservice.springboot.model.User;
 import se.chriskevin.microservice.springboot.model.WritableUserDetails;
 
 @Service
-@NoArgsConstructor
 @AllArgsConstructor
 public class UserService {
 
-  @Autowired private UserStore userStore;
+  private UserStore userStore;
 
   public List<User> getUsers() {
     return userStore.getUsers();
   }
 
-  public Option<User> getUser(final UUID id) {
-    return userStore.getUsers().find(user -> user.getId().equals(id));
+  public Option<User> getUser(final UUID uuid) {
+    return userStore.getUser(uuid);
   }
 
-  public void deleteUser(final UUID uuid) {
-    userStore.removeUser(uuid);
+  public Option<User> deleteUser(final UUID uuid) {
+    return userStore.removeUser(uuid);
   }
 
   public User addUser(final WritableUserDetails userDetails) {
     return userStore.addUser(new User(UUID.randomUUID(), userDetails.getUsername()));
+  }
+
+  public Option<User> updateUser(final UUID uuid, final WritableUserDetails userDetails) {
+    return userStore
+        .getUser(uuid)
+        .map(user -> user.withUsername(userDetails.getUsername()))
+        .flatMap(user -> userStore.updateUser(uuid, user));
   }
 }
